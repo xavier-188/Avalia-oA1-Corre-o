edb863347a999c4ca1c035885de7e030388fef40
Enzo <enzoxavierbiscaia@gmail.com>
2025-12-04 18:11:38 -0300

    API completa

---
+++ API/Migrations/20251204205633_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    TarefaId = table.Column<string>(type: "TEXT", nullable: false),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.TarefaId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CriadoEm", "Status", "Titulo" },
+                values: new object[,]
+                {
+                    { "2f1b7dc1-3b9a-4e1a-a389-7f5d2f1c8f3e", new DateTime(2025, 12, 7, 17, 56, 33, 502, DateTimeKind.Local).AddTicks(2302), "Não iniciada", "Estudar Angular" },
+                    { "6a8b3e4d-5e4e-4f7e-bdc9-9181e456ad0e", new DateTime(2025, 12, 11, 17, 56, 33, 502, DateTimeKind.Local).AddTicks(2295), "Não iniciada", "Concluir relatório" },
+                    { "e5d4a7b9-1f9e-4c4a-ae3b-5b7c1a9d2e3f", new DateTime(2025, 12, 18, 17, 56, 33, 502, DateTimeKind.Local).AddTicks(2306), "Não iniciada", "Passeio no parque" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -31,19 +31,33 @@ app.MapPost("/api/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromB
 //PUT: http://localhost:5273/tarefas/alterar/{id}
 app.MapPut("/api/tarefas/alterar/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    //Implementar a alteração do status da tarefa
+    Tarefa? tarefa = ctx.Tarefas.Find(id);
+
+    if(tarefa.Status == "Não iniciada")
+    {
+        tarefa.Status = "Em andamento";
+    }
+    else if(tarefa.Status == "Em andamento")
+    {
+        tarefa.Status = "Concluída";
+    }
+
+    ctx.Tarefas.Update(tarefa);
+    ctx.SaveChanges();
+    return Results.Ok(tarefa);
+    
 });
 
 //GET: http://localhost:5273/tarefas/naoconcluidas
 app.MapGet("/api/tarefas/naoconcluidas", ([FromServices] AppDataContext ctx) =>
 {
-    //Implementar a listagem de tarefas não concluídas
+    return Results.Ok(ctx.Tarefas.Where(x => x.Status != "Concluída"));
 });
 
 //GET: http://localhost:5273/tarefas/concluidas
 app.MapGet("/api/tarefas/concluidas", ([FromServices] AppDataContext ctx) =>
 {
-    //Implementar a listagem de tarefas concluídas
+    return Results.Ok(ctx.Tarefas.Where(x => x.Status == "Concluída"));
 });
 
 app.Run();
